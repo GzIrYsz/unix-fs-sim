59d67bc0621d163e78c9c27a1f145b764c0928b0
Thomas REMY <contact.t.remy777@gmail.com>
2024-03-29 02:56:45 +0100

    feat: Implement CRUD operations for blocks
    Add functions to create, read, update, and delete blocks within a partition. Include error handling and logging for each operation.

--- src/models/low_level/block.h
+++ src/models/low_level/block.h
@@ -1,10 +1,48 @@
 /**
  * @file block.h
- * @brief
+ * @brief This file contains the CRUD operations available on a block.
  * @author Thomas REMY
  * @author Pierre FRANCK-PAPUCHON
- * @version 0.1.0
+ * @version 1.0.0
  * @date 03-28-2024
  */
 
 #pragma once
+
+#include "unix_fs_sim/ufs.h"
+
+#include "ufs.priv.h"
+
+/**
+ * @brief Creates a block at the specified location.
+ * @param p The partition where to create the block.
+ * @param i The index of the block you want to create.
+ * @return 0 if everything went well, -1 otherwise.
+ */
+int create_block(partition_stat_t p, unsigned int i);
+
+/**
+ * @brief Reads the data located at the specified block.
+ * @param p The partition where to read the data.
+ * @param buf The buffer where to store the data.
+ * @param i The index of the block to read.
+ * @return 0 if everything went well, -1 otherwise.
+ */
+int read_block(partition_stat_t p, void *buf, unsigned int i);
+
+/**
+ * @brief Writes new data on the bloc located at the specified index.
+ * @param p The partition where to write the data.
+ * @param buf The buffer where the data is stored (data will not be modified).
+ * @param i The index of the block to update.
+ * @return 0 if everything went well, -1 otherwise.
+ */
+int update_block(partition_stat_t p, const void *buf, unsigned int i);
+
+/**
+ * @brief Deletes the bloc located at the specified index.
+ * @param p The partition where to delete the block.
+ * @param i The index of the block to delete.
+ * @return 0 if everything went well, -1 otherwise.
+ */
+int delete_block(partition_stat_t p, unsigned int i);
