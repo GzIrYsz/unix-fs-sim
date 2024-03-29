cc51278f9f411da126ecd07a4ae945d777e7f597
Thomas REMY <contact.t.remy777@gmail.com>
2024-03-29 23:31:25 +0100

    feat: Add CRUD operations for inode bitmap
    Added functions to create, read, update, and delete inode bitmaps. Also included a function to find the next free inode index.

--- src/models/mid_level/inode_bitmap.h
+++ src/models/mid_level/inode_bitmap.h
@@ -1,10 +1,48 @@
 /**
  * @file inode_bitmap.h
- * @brief
+ * @brief This file contains the CRUD operations available on an inode bitmap.
  * @author Thomas REMY
- * @author Pierre FRANCK-PAPUCHON
- * @version 0.1.0
- * @date 03-28-2024
+ * @version 1.0.0
+ * @date 03-29-2024
  */
 
 #pragma once
+
+#include <stdint.h>
+
+#include "../../ufs.priv.h"
+
+/**
+ * @brief Creates a new inode bitmap.
+ * @param p The partition where to create de bitmap.
+ * @return 0 if everything went well, -1 otherwise.
+ */
+int create_inodebitmap(partition_t p);
+
+/**
+ * @brief Reads the inode bitmap and store it in memory.
+ * @param p The partition.
+ * @return 0 if everything went well, -1 otherwise.
+ */
+int read_inodebitmap(partition_t p);
+
+/**
+ * @brief Writes the inode bitmap on the disk.
+ * @param p The partition.
+ * @return 0 if everything went well, -1 otherwise.
+ */
+int update_inodebitmap(partition_t p);
+
+/**
+ * @brief Deletes the inode bitmap stored on disk.
+ * @param p The partition.
+ * @return 0 if everything went well, -1 otherwise.
+ */
+int delete_inodebitmap(partition_t p);
+
+/**
+ * @brief Finds the next free inode and returns its index.
+ * @param p The partition.
+ * @return The index of the next free inode or -1 if an error occurs.
+ */
+uint32_t next_free_inode(partition_t p);
