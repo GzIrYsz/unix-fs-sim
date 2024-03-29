b85f8fb03fc3003083fb34451986117853c9bb6b
Thomas REMY <contact.t.remy777@gmail.com>
2024-03-29 16:21:42 +0100

    Refactor inode.h to include CRUD operations
    Added CRUD operations for creating, reading, updating, and deleting inodes in the inode.h file. Updated version to 1.0.0 and added necessary includes for functionality.

--- src/models/mid_level/inode.h
+++ src/models/mid_level/inode.h
@@ -1,10 +1,47 @@
 /**
  * @file inode.h
- * @brief
+ * @brief This file contains the CRUD operations available on an inode.
  * @author Thomas REMY
- * @author Pierre FRANCK-PAPUCHON
- * @version 0.1.0
- * @date 03-28-2024
+ * @version 1.0.0
+ * @date 03-29-2024
  */
 
 #pragma once
+
+#include "unix_fs_sim/ufs.h"
+
+#include "../../ufs.priv.h"
+
+/**
+ * @brief Creates an inode at the specified location.
+ * @param p The partition to use.
+ * @param i The index of the inode you want to create.
+ * @return 0 if everything went well, -1 otherwise.
+ */
+int create_inode(partition_t p, uint32_t i);
+
+/**
+ * @brief Reads an inode located at the specified location.
+ * @param p The partition to use.
+ * @param inode The inode variable where to store the data.
+ * @param i The index of inode to read.
+ * @return 0 if everything went well, -1 otherwise.
+ */
+int read_inode(partition_t p, inode_t *inode, uint32_t i);
+
+/**
+ * @brief Writes or updates the inode located at the specified index.
+ * @param p The partition to use.
+ * @param inode The inode to store on the disk.
+ * @param i The index of the inode to update.
+ * @return 0 if everything went well, -1 otherwise.
+ */
+int update_inode(partition_t p, inode_t inode, uint32_t i);
+
+/**
+ * @brief Deletes the inode located at the specified index.
+ * @param p The partition to use.
+ * @param i The index of the inode to delete.
+ * @return 0 if everything went well, -1 otherwise.
+ */
+int delete_inode(partition_t p, uint32_t i);
