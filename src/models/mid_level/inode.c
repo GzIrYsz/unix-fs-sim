b27fc87296687f69aafbb0e29a996f71de48d5c8
Pierre FRANCK-PAPUCHON <pierrefranck78@hotmail.com>
2024-03-31 00:59:39 +0100

    Fix inode

--- src/models/mid_level/inode.c
+++ src/models/mid_level/inode.c
@@ -20,16 +20,21 @@
 extern logger_t* logger;
 
 off_t get_offset_inode(partition_t p, uint32_t i){
-    uint32_t nb_data = p.super_bloc.nb_data;
-    uint32_t block_size = p.super_bloc.block_size;
-    uint32_t nb_inodes = p.super_bloc.nb_inodes;
-    return (int) (ceil(nb_data/block_size)+ceil(nb_inodes/block_size)+1)*block_size + i * sizeof(inode_t);
+    double nb_data = (double) p.super_bloc.nb_data;
+    double block_size = (double) p.super_bloc.block_size;
+    double nb_inodes = (double) p.super_bloc.nb_inodes;
+    //TODO Look if you don't get a typo error
+    return (off_t) (ceil(nb_data/block_size)+ceil(nb_inodes/block_size)+1)*block_size + i * sizeof(inode_t);
 }
 
 int create_inode(partition_t p, uint32_t i){
     if (i > p.super_bloc.nb_blocks) {
         logger->error("You are trying to create an inode beyond the memory for all inode.");
         return -1;
+    }
+
+    if (p.inode_bitmap[i] == 1){
+        logger->error("You are trying to create an already create inode");
     }
 
     p.inode_bitmap[i] = 1;
@@ -62,7 +67,7 @@ int read_inode(partition_t p, inode_t* inode, uint32_t i){
         return -1;
     }
     for (int j = 0; j < 8; j++) {
-        if(read(p.fd, inode->data_blocks+i, 8) == -1){
+        if(read(p.fd, inode->data_blocks+j, sizeof(uint32_t)) == -1){
             logger->error("An error occurred when trying to read one of your inode data block");
             return -1;
         }
@@ -80,20 +85,20 @@ int update_inode(partition_t p, inode_t inode, uint32_t i){
 
     off_t inode_pos = get_offset_inode(p, i);
 
-    if(write(p.fd, &inode.memory_size_data, 8) == -1){
+    if(write(p.fd, &inode.memory_size_data, 4) == -1){
         logger->error("An error occurred when trying to update the size of inode data");
         return -1;
     }
-    if(write(p.fd, &inode.last_modification, 8) == -1){
+    if(write(p.fd, &inode.last_modification, 4) == -1){
         logger->error("An error occurred when trying to update the size of inode last modification");
         return -1;
     }
-    if(write(p.fd, &inode.last_access, 8) == -1){
+    if(write(p.fd, &inode.last_access, 4) == -1){
         logger->error("An error occurred when trying to update the size of inode last access");
         return -1;
     }
     for (int j = 0; j < 8; j++) {
-        if(write(p.fd, &inode.data_blocks+i, 8) == -1){
+        if(write(p.fd, &inode.data_blocks+i, 4) == -1){
             logger->error("An error occurred when trying to read one of your inode data block");
             return -1;
         }
@@ -106,6 +111,11 @@ int update_inode(partition_t p, inode_t inode, uint32_t i){
 int delete_inode(partition_t p, uint32_t i){
     if (i > p.super_bloc.nb_blocks){
         logger->error("You are trying to create an inode beyond the memory for all inode.");
+        return -1;
+    }
+
+    if (p.inode_bitmap[i] == 0){
+        logger->error("You are trying to delete a non-existent inode");
         return -1;
     }
 
