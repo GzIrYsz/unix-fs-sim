380f6b1a6fcc302dc7a97aefd7b6df101844566e
Pierre FRANCK-PAPUCHON <pierrefranck78@hotmail.com>
2024-04-01 19:39:48 +0200

    directory.h

--- src/models/high_level/directory.h
+++ src/models/high_level/directory.h
@@ -7,4 +7,18 @@
  * @date 03-28-2024
  */
 
+#include "../low_level/block.h"
 #pragma once
+
+
+int create_directory(partition_t p);
+
+int read_directory(partition_t p);
+
+int update_directory(partition_t p);
+
+int delete_directory(partition_t p);
+
+int insertion_entry(partition_t p, dir_entry_t dir);
+
+int delete_entry(partition_t p, dir_entry_t dir);
